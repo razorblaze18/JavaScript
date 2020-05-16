26d9795170216f8d3ba5374dd3f701bda373a1ae
razor_blaze <dhangar.mudit3@gmail.com>
2020-05-16 20:44:50 +0530

    master

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,40 +1,113 @@
-const productList = {
-    product: [
-        {
-        title: 'A Pillow',
-        imageUrl: 'https://www.fairfieldstore.com/images/products/lrg/fairfield-store-ffi-108-s-down-alternative-eco-pillow_lrg.jpg',
-        price: 19.99,
-        description: 'A soft Pillow!'
-        },
-        {
-            title: 'A Carpet',
-            imageUrl: 'https://assets.jassets.com/dpr_2/f_auto/fl_progressive/h_403,q_70,w_295/v1/assets/images/7721426/2018/11/1/d181034f-47b9-427f-98bf-875205ea8e3c1541052991414-IMPERIAL-KNOTS-CHARCOAL-TEAL-PERSIAN-HAND-TUFTED-CARPET-9651-1.jpg',
-            price: 23.99,
-            description: 'A carpet which you might like.',
-        }
-    ],
-    render () {
-        const renderHook = document.getElementById('app');
+class Product {
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
+
+    constructor(title, image, desc, price) {
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+
+class ShoppingCart {
+    items = [];
+
+    addProduct(products){
+        this.items.push(products);
+        this.totalOutput = `<h2>Total: \$${1}</h2>`;
+    }
+
+    render() {
+        const cartEl = document.createElement('section');
+        cartEl.innerHTML = `
+        <h2>Total: \$${0}</h2>
+        <button>Order Now!</button>
+        `;
+        cartEl.className = 'cart';
+        this.totalOutput = cartEl.querySelector('h2');
+        return cartEl;
+    }
+}
+
+class ProductItem {
+    constructor(products) {
+        this.products = products;
+    }
+
+    addToCart() {
+        console.log('Adding product to Cart....');
+        console.log(this.products);
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+        prodEl.className = 'product-item';
+        prodEl.innerHTML = `
+        <div>
+            <img src ="${this.products.imageUrl}" alt="${this.products.title}">
+            <div class="product-item__content">
+                <h2>${this.products.title}</h2>
+                <h3>\$${this.products.price}</h3>
+                <p>${this.products.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this))
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
+        new Product(
+            'A Pillow',
+            'https://www.fairfieldstore.com/images/products/lrg/fairfield-store-ffi-108-s-down-alternative-eco-pillow_lrg.jpg',
+            'A soft Pillow!',
+            19.99
+        ),
+
+        new Product(
+            'A Carpet',
+            'https://assets.jassets.com/dpr_2/f_auto/fl_progressive/h_403,q_70,w_295/v1/assets/images/7721426/2018/11/1/d181034f-47b9-427f-98bf-875205ea8e3c1541052991414-IMPERIAL-KNOTS-CHARCOAL-TEAL-PERSIAN-HAND-TUFTED-CARPET-9651-1.jpg',
+            'A carpet which you might like.',
+            23.99
+        )
+
+    ];
+
+    constructor() { }//This is known as Method
+
+    render() {
+        
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
-        for (const prod of this.product){
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-            <div>
-                <img src ="${prod.imageUrl}" alt="${prod.title}">
-                <div class="product-item__content">
-                    <h2>${prod.title}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            `;
+        for (const prod of this.products) {
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
-        renderHook.append(prodList);
+        return prodList;
     }
-};
+}
 
-productList.render();
+class Shop {
+    render() {
+        const renderHook = document.getElementById('app');
+
+        const cart = new ShoppingCart();
+        const cartEl = cart.render();
+        const productList = new ProductList();
+        const prodListEl = productList.render();
+
+        renderHook.append(cartEl);
+        renderHook.append(prodListEl);
+    }
+}
+
+const shop = new Shop();
+shop.render();
