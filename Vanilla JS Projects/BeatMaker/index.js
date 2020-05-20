8addc0e472a8bc7a02b3966e1a0a33430e306d96
razor_blaze <dhangar.mudit3@gmail.com>
2020-05-20 14:18:52 +0530

    master

---
+++ Vanilla JS Projects/BeatMaker/index.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");//All the div inside the pads.
+    const visual = document.querySelector('.visual');
+    const color = [
+        "#60d391",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#4618a9",
+        "#3ea3d1"
+    ];
+
+    //Lets get going with the sound here
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;//Reset Time
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+
+    //Create a function that makes bubbles.
+    const createBubbles = (index) => {
+        const bubbles = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+    };
+});
+
