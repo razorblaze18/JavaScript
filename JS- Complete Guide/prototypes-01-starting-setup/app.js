5980859fde872041abe5c24b41014466ffd2c689
razor_blaze <dhangar.mudit3@gmail.com>
2020-05-22 00:13:26 +0530

    master

--- JS- Complete Guide/prototypes-01-starting-setup/app.js
+++ JS- Complete Guide/prototypes-01-starting-setup/app.js
@@ -1,7 +1,14 @@
-// class Person {
+// class AgedPerson{
+//     printAge(){
+//         console.log(this.age);
+//     }
+// }
+
+// class Person extends AgedPerson{
 //     name = 'Aman';
 
 //     constructor() {
+//         super();
 //         this.age = 30;
 //     }
 
@@ -18,13 +25,19 @@ function Person() {
     };
 }
 
-Person.prototype = {
-    printAge() {
-        console.log(this.age);
-    }
+// Person.prototype = {
+//     printAge() {
+//         console.log(this.age);
+//     }
+// };
+
+Person.prototype.printAge = function() {
+    console.log(this.age);
 };
 
 const person = new Person();
 person.greet();
 person.printAge();
 console.log(person.__proto__);
+const person2 = new person.__proto__.constructor();
+console.log(person2);
