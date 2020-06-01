7c0bfc925a032b177693620ac53c08538e6cb458
razor_blaze <dhangar.mudit3@gmail.com>
2020-06-01 16:09:22 +0530

    Learned about async and sync code

--- JS- Complete Guide/async-01-starting-setup/app.js
+++ JS- Complete Guide/async-01-starting-setup/app.js
@@ -1,15 +1,40 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+
+
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(
+    posData => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
+  }, error => {
+    console.log(error);
+  }
+  );
+  setTimeout(() => {
+    console.log('Timer done!');
+  }, 0);
+  console.log('Getting Position...');
 }
 
 button.addEventListener('click', trackUserHandler);
 
-let result = 0;
+// let result = 0;
 
-for( let i = 0; i < 100000000 ;i++){
-  result += i;
-}
-console.log(result);
+// for( let i = 0; i < 100000000 ;i++){
+//   result += i;
+// }
+// console.log(result);
+
+// const greet = () => {
+//   console.log('Hi');
+// };
+
+// const showAlert = () => {
+//   alert('Danger!');
+// };
+
+// setTimeout(showAlert, 2000);
+// greet();
