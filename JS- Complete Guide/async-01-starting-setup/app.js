188508d1aff35dcbb80d2ce5f5c1302a833b89c3
razor_blaze <dhangar.mudit3@gmail.com>
2020-06-02 05:01:21 +0530

    Advance concepts in string and learn about Tagged template in strings

--- JS- Complete Guide/async-01-starting-setup/app.js
+++ JS- Complete Guide/async-01-starting-setup/app.js
@@ -1,21 +1,40 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(success => {
+      resolve(success);
+    }, error => {
+  
+    }, opts);
+  });
+  return promise;
+  };
+  
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  }); //A Promise is in the end an object with the functionality or which the idea of having such a then method which you call on it
+  return promise;
+};
 
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     posData => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then(data => {
+        console.log(data, posData);
+      });//This then attribute works only when the promises resolves.
   }, error => {
     console.log(error);
   }
   );
-  setTimeout(() => {
+  setTimer(1000).then(() => {
     console.log('Timer done!');
-  }, 0);
+  })
   console.log('Getting Position...');
 }
 
--- JS- Complete Guide/nums-strings-01-starting-setup/app.js
+++ JS- Complete Guide/nums-strings-01-starting-setup/app.js
@@ -1,3 +1,21 @@
 function randomIntBetween(min, max){
-    return Math.random() * max + min;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}//Here, we add 1 because random number produces the number between 0 and 1 and the maximum it will yeild is 0.999 adding 1 to it will give the maximum number and to round off the number to lie under the give range we wrap the code with Math.floor.
+console.log(randomIntBetween(1,5));
+
+//Tagged Template in Strings
+function productDescription(string, productName, productPrice){
+    console.log(string);
+    console.log(productName);
+    console.log(productPrice);
+    let priceCategory = 'cheap';
+    if (productPrice > 20){
+        priceCategory = 'fairly priced';
+    }
+    return `${string[0]}${productName}${string[1]}${priceCategory}${string[2]}`;
 }
+const prodName = 'JavaScript Course';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product ${prodName} is ${prodPrice}.`;
+console.log(productOutput);
