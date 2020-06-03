9fc4decba0b2274712493a71befb507449aacf00
razor_blaze <dhangar.mudit3@gmail.com>
2020-06-03 15:07:56 +0530

    Day2: : Learn about State and Props

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -3,12 +3,37 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Aman', age: 22 },
+      { name: 'Mudit', age: 21 },
+      { name: 'Chundi', age: 17 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('was clicked!');
+    //DON'T DO THIS: this.state.persons[0].name = 'Aman Dhangar';
+    this.setState(
+      {
+        persons: [
+          { name: 'Aman Dhangar', age: 22 },
+          { name: 'Mudit', age: 21 },
+          { name: 'Ojaswita', age: 17 }
+        ]
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!</p>
-        <Person/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));//Create ELement takes minimun 3 arguements and maximum infinite arguements.
